6dd1499a07f294a5ffc8ed0cf49163c7de9e7bb3
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2021-10-25 13:56:13 -0400

    0010-fix-tcp-death-detection
    Change-Id: Ie0ab5a8ec7c1a4d43d6f4d646ae133280898091e

--- pjlib/include/pj/sock.h
+++ pjlib/include/pj/sock.h
@@ -324,6 +324,7 @@ extern const pj_uint16_t PJ_SO_KEEPALIVE;
 extern const pj_uint16_t PJ_TCP_KEEPIDLE;
 extern const pj_uint16_t PJ_TCP_KEEPINTVL;
 extern const pj_uint16_t PJ_TCP_KEEPCNT;
+extern const pj_uint16_t PJ_TCP_USER_TIMEOUT;
 
 /** Set the protocol-defined priority for all packets to be sent on socket.
  */
@@ -366,6 +367,9 @@ extern const pj_uint16_t PJ_IP_DROP_MEMBERSHIP;
 
     /** Get #PJ_TCP_KEEPINTVL constant */
 #   define pj_TCP_KEEPINTVL() PJ_TCP_KEEPINTVL(void);
+
+    /** Get #PJ_TCP_USER_TIMEOUT constant */
+    PJ_DECL(pj_uint16_t) PJ_TCP_USER_TIMEOUT(void);
 
     /** Get #PJ_TCP_KEEPCNT constant */
 #   define pj_TCP_KEEPCNT() PJ_TCP_KEEPCNT(void);
@@ -411,6 +415,9 @@ extern const pj_uint16_t PJ_IP_DROP_MEMBERSHIP;
 
     /** Get #PJ_TCP_KEEPIDLE constant */
 #   define pj_TCP_KEEPIDLE() PJ_TCP_KEEPIDLE
+
+    /** Get #PJ_TCP_USER_TIMEOUT constant */
+#   define pj_TCP_USER_TIMEOUT() PJ_TCP_USER_TIMEOUT
 
     /** Get #PJ_TCP_KEEPINTVL constant */
 #   define pj_TCP_KEEPINTVL() PJ_TCP_KEEPINTVL
