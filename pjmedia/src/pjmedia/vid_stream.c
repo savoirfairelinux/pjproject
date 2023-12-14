b1c6d56d346bad3b1dd73718cbc54e8cf252352c
sauwming <ming@teluu.com>
2023-12-14 12:15:58 +0800

    Fixed deadlock between stream and ICE (#3801)
    Change-Id: I78a3fe6d2240c44ad2fb372b71d09cdf4f616125

--- pjmedia/src/pjmedia/vid_stream.c
+++ pjmedia/src/pjmedia/vid_stream.c
@@ -584,6 +584,13 @@ static pj_status_t send_rtcp(pjmedia_vid_stream *stream,
     int len, max_len;
     pj_status_t status;
 
+
+    /* To avoid deadlock with media transport, we use the transport's
+     * group lock.
+     */
+    if (stream->transport->grp_lock)
+        pj_grp_lock_acquire( stream->transport->grp_lock );
+
     /* Build RTCP RR/SR packet */
     pjmedia_rtcp_build_rtcp(&stream->rtcp, &sr_rr_pkt, &len);
 
@@ -664,6 +671,10 @@ static pj_status_t send_rtcp(pjmedia_vid_stream *stream,
             stream->rtcp_tx_err_cnt = 0;
         }
     }
+
+    if (stream->transport->grp_lock)
+        pj_grp_lock_release( stream->transport->grp_lock );
+
     return status;
 }
 
