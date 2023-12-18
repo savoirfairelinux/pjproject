d63b95d88d687bad5d2e89d058e58e18d9116fd2
sauwming <ming@teluu.com>
2023-12-18 11:56:43 +0800

    Prevent immediate tsx termination upon transport error (#3805)
    Change-Id: I5cdc1609395753cd78011e072670d70b9b042c11

--- pjsip/src/pjsip/sip_transaction.c
+++ pjsip/src/pjsip/sip_transaction.c
@@ -2039,20 +2039,28 @@ static void send_msg_callback( pjsip_send_state *send_state,
             else
                 sc = PJSIP_SC_TSX_TRANSPORT_ERROR;
 
-            /* Terminate transaction, if it's not already terminated. */
-            tsx_set_status_code(tsx, sc, &err);
-            if (tsx->state != PJSIP_TSX_STATE_TERMINATED &&
+            /* We terminate the transaction for 502 error. For 503,
+             * we will retry it.
+             * See https://github.com/pjsip/pjproject/pull/3805
+             */
+            if (sc == PJSIP_SC_BAD_GATEWAY &&
+                tsx->state != PJSIP_TSX_STATE_TERMINATED &&
                 tsx->state != PJSIP_TSX_STATE_DESTROYED)
             {
-                tsx_set_state( tsx, PJSIP_TSX_STATE_TERMINATED, 
+                tsx_set_status_code(tsx, sc, &err);
+                /* Set tsx state to TERMINATED, but release the lock
+                 * when invoking the callback, to avoid deadlock.
+                 */
+                tsx_set_state( tsx, PJSIP_TSX_STATE_TERMINATED,
                                PJSIP_EVENT_TRANSPORT_ERROR,
-                               send_state->tdata, 0);
+                               send_state->tdata, RELEASE_LOCK);
             } 
             /* Don't forget to destroy if we have pending destroy flag
              * (https://github.com/pjsip/pjproject/issues/906)
              */
             else if (tsx->transport_flag & TSX_HAS_PENDING_DESTROY)
             {
+                tsx_set_status_code(tsx, sc, &err);
                 tsx_set_state( tsx, PJSIP_TSX_STATE_DESTROYED, 
                                PJSIP_EVENT_TRANSPORT_ERROR,
                                send_state->tdata, 0);
